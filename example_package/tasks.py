651de0d22b6a06952c0d6292f671befe133c5723
Vladimir Antonovich <vladimir_antonovich@cloudify.co>
2020-01-15 15:05:26 +0300

    add resumable operations

--- example_package/tasks.py
+++ example_package/tasks.py
@@ -1,3 +1,7 @@
+from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
+from datetime import datetime, timedelta
+from time import sleep
 from cloudify.decorators import operation
 
 
@@ -5,3 +9,13 @@ from cloudify.decorators import operation
 def write_to_file(*args, **kwargs):
     with open('/tmp/cloudify-plugin', 'w') as f:
         f.write('This simple example gives you the power to do amazing things.\n')
+
+@operation(resumable=True)
+def tick():
+    test_time = int(inputs.get('test_time', 30))
+    end_time = datetime.now() + timedelta(0,test_time)
+    while True:
+        ctx.logger.info("test_func: {}".format(datetime.now()))
+        if datetime.now() >= end_time:
+            return
+        sleep(1)
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='cloudify-plugin-example',
-    version='0.0.1',
+    version='0.0.2',
     description='Writes a file',
     packages=['example_package'],
     install_requires=[
-        "cloudify-plugins-common>=4.0"
+        "cloudify-plugins-common>=5.0.5"
     ]
 )
